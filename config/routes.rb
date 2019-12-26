4e636fc3eda3c4f9daa1734698bb344b09fa3a67
nguyenhop14 <nguyenhop0104@gmail.com>
2019-12-26 14:56:02 +0700

    Log in, log out user with gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  #devise_for :users
+  devise_for :users, :controllers => { :registrations => "users/registrations" }
   root to: "static_pages#index"
   get "/about", to: "static_pages#about"
+  resources :users
 end
